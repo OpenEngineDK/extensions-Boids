7e41a4b7c4bc43b11007316ff573238a0806d7e3
Peter Kristensen <ptx@openengine.dk>
2011-01-09 17:07:31 +0000

    Random position

--- Animations/Boid.cpp
+++ Animations/Boid.cpp
@@ -1,11 +1,19 @@
 #include "Boid.h"
 
+#include <Math/RandomGenerator.h>
+
 namespace OpenEngine {
 namespace Animations {
-
+    
+    using namespace Math;
 
     Boid::Boid(ISceneNode* node) {
         transformationNode = new TransformationNode();
+        RandomGenerator rg;
+        rg.SeedWithTime();
+        transformationNode->Move(rg.UniformFloat(-20,20),
+                                 rg.UniformFloat(-20,20),
+                                 rg.UniformFloat(-20,20));
         transformationNode->AddNode(node);
     }
 
